bb06f2a52754edf6b0e5bcd876fac9a548ed4d2c
romanvozbrannyi <romanvozbrannyi@gmail.com>
2023-06-07 16:04:19 +0200

    feat: ChainRisk balance info

--- src/components/common/balanceInfo/BalanceInfo.tsx
+++ src/components/common/balanceInfo/BalanceInfo.tsx
@@ -1,10 +1,11 @@
-import { Box, Button, Stack, Text } from '@chakra-ui/react';
+import { Box, Button, Stack, Text, Tooltip } from '@chakra-ui/react';
 import { useAccount, useContractRead } from 'wagmi';
 import { nftABI, nftContractAddress } from '../../../utils/nftABI.ts';
 import { useState } from 'react';
 import { formatEther } from 'viem';
 import BalanceModal from '../balanceModal/BalanceModal.tsx';
 import WithdrawModal from '../withdrawModal/WithdrawModal.tsx';
+import { QuestionOutlineIcon } from '@chakra-ui/icons';
 
 const BalanceInfo = () => {
   const { address } = useAccount();
@@ -33,12 +34,18 @@ const BalanceInfo = () => {
         <Box flex={1}>
           <Box maxWidth={700}>
             <Text colorScheme="gray" fontWeight="bold">
-              Balance:{' '}
+              ChainRisk balance:{' '}
               {balanceIsLoading
                 ? 'Loading...'
                 : `${
                     Math.round(parseFloat(formatEther(BigInt(balance || 0))) * 100) / 100
                   } LINK`}
+              <Tooltip
+                hasArrow
+                label="The ChainRisk contract uses LINK to retrieve your Credit Rating. Please ensure you have approved LINK spending and transferred enough LINK tokens to Mint or Update Score."
+              >
+                <QuestionOutlineIcon ml={1} mb={0.5} color="gray.500" />
+              </Tooltip>
             </Text>
             <Text fontSize="sm" color="gray.600">
               Minting or updating an NFT costs <b>0.1 LINK</b>
